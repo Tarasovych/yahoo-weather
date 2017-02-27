3559ad0ea7555b9b525472949852eeb625666bd8
taras <tarasovych.ua@gmail.com>
2017-02-27 23:51:30 +0200

    1

--- weather.php
+++ weather.php
@@ -3,4 +3,6 @@
     $api_request = "http://query.yahooapis.com/v1/public/yql?q=" . urlencode($query) . "&format=json";
     $result = file_get_contents($api_request); //получаем данные по url
     $result_decode = json_decode($result); // декодируем json строку
-    echo var_dump($result_decode); //выводим через var_dump для наглядности
+    // echo var_dump($result_decode); //выводим через var_dump для наглядности
+    echo '<pre>' . var_export($result_decode->query->results->channel->item->description, true) . '</pre>';
+    // echo '<pre>' . var_export(array_keys($result,description)) . '</pre>';
