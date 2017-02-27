af1c8fc9a377841cf64ec763128c63bea577a442
taras <tarasovych.ua@gmail.com>
2017-02-27 16:40:08 +0200

    1

---
+++ weather.php
@@ -0,0 +1,6 @@
+<?php
+    $query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="nome, ak")';
+    $api_request = "http://query.yahooapis.com/v1/public/yql?q=" . urlencode($query) . "&format=json";
+    $result = file_get_contents($api_request); //получаем данные по url
+    $result_decode = json_decode($result); // декодируем json строку
+    echo var_dump($result_decode); //выводим через var_dump для наглядности
